5e2c17105f10ce0cf3ea27bb27580e2641875126
TRAN DUC LAM <t-lam@plott.co.jp>
2023-02-10 17:03:40 +0700

    add migratejs + mock api

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {ConfigProvider} from 'antd';
 import enUS from 'antd/locale/en_US';
@@ -6,7 +6,21 @@ import BaseRoutes from './routes';
 import 'antd/dist/reset.css';
 import './i18next';
 
+import { createServer } from "miragejs"
+
+// let server = createServer()
+// server.get("/api/users", { users: [{ id: 1, name: "Bob" }] })
+
 function App() {
+	let [users, setUsers] = useState([])
+	useEffect(() => {
+		fetch("/api/users")
+			.then((res) => res.json())
+			.then((json) => {
+				console.log(json.users);
+				setUsers(json.users)
+			})
+	}, [])
 	return (
 		<div className="App">
 			<BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 import App from './app/app';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import {makeServer} from './api/mock/server';
+
+if (process.env.NODE_ENV === "development") {
+	makeServer({environment: "development"})
+}
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App/>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
